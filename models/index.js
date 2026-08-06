65e733d3f90c5368b9eda15e754413eb6bf92e4e
divya <pulamidivya26@gmail.com>
2026-08-06 11:06:19 -0400

    commit added models

--- models/index.js
+++ models/index.js
@@ -2,17 +2,17 @@
 // Lets the rest of the app grab them from here: const { Task } = require('./models')
 
 const db = require('../db');
-const Task = require('./task.model');
-const User = require('./user.model');
+const MoodPin = require('./moodPin');
+const User = require('./user');
 
 // ---------- associations ----------
 // Describe how tables relate here. When you're ready to tie tasks to their
 // owner, uncomment these (it adds a userId column to tasks):
-//   User.hasMany(Task)     // one user has many tasks
-//   Task.belongsTo(User)   // each task belongs to one user (adds a userId column)
+  User.hasMany(MoodPin)     // one user has many MoodPin
+  MoodPin.belongsTo(User)   // each MoodPin belongs to one user (adds a userId column)
 
 module.exports = {
   db, // exported too so seed.js can sync from one place
-  Task,
+  MoodPin,
   User,
 };
